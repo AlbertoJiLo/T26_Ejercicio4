1840a6ec5079859fad74cc696ae613f0ba12f324
AlbertoJiLo <alberto_tgn_14@hotmail.com>
2022-11-14 21:51:59 +0100

    1

---
+++ src/main/java/com/example/T26/T26Ejercicio1Application.java
@@ -0,0 +1,13 @@
+package com.example.T26;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T26Ejercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T26Ejercicio1Application.class, args);
+	}
+
+}
